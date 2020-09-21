0a03308759453f76f186441b07dc4001c7b860e6
xiaoxiong <2509665154@qq.com>
2020-09-21 23:58:16 +0800

    第一次提交，测试一

---
+++ src/main/java/com/yida/gitdemo1/Gitdemo1Application.java
@@ -0,0 +1,13 @@
+package com.yida.gitdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitdemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yida/gitdemo1/Gitdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yida.gitdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitdemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
